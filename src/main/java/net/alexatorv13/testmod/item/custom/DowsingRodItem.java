e3b585d869d974895fb865b76acee75cd93e41e1
ALEXATOR <alexander.regalado@ikasle.egibide.org>
2022-11-05 09:57:43 +0100

    tags añadidos y otras cosas mas

--- src/main/java/net/alexatorv13/testmod/item/custom/DowsingRodItem.java
+++ src/main/java/net/alexatorv13/testmod/item/custom/DowsingRodItem.java
@@ -1,17 +1,27 @@
 package net.alexatorv13.testmod.item.custom;
 
+import net.alexatorv13.testmod.util.ModTags;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class DowsingRodItem extends Item {
-    public DowsingRodItem(Properties pProperties){
+    public DowsingRodItem(Properties pProperties) {
         super(pProperties);
     }
 
@@ -19,22 +29,22 @@ public class DowsingRodItem extends Item {
     @Override
     public InteractionResult useOn(UseOnContext pContext) {
 
-        if(pContext.getLevel().isClientSide()) {
+        if (pContext.getLevel().isClientSide()) {
             BlockPos positionClicked = pContext.getClickedPos();
             Player player = pContext.getPlayer();
             boolean foundBlock = false;
 
-            for(int i = 0; i <= positionClicked.getY() + 64; i++) {
+            for (int i = 0; i <= positionClicked.getY() + 64; i++) {
                 Block blockBelow = pContext.getLevel().getBlockState(positionClicked.below(i)).getBlock();
 
-                if(isValuableBlock(blockBelow)) {
+                if (isValuableBlock(blockBelow)) {
                     outputValuableCoordinates(positionClicked.below(i), player, blockBelow);
                     foundBlock = true;
                     break;
                 }
             }
 
-            if(!foundBlock) {
+            if (!foundBlock) {
                 player.sendMessage(new TranslatableComponent("item.tutorialmod.dowsing_rod.no_valuables"),
                         player.getUUID());
             }
@@ -47,14 +57,24 @@ public class DowsingRodItem extends Item {
     }
 
 
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        if (Screen.hasShiftDown()) {
+            pTooltipComponents.add(new TranslatableComponent("tooltip.testmod.dowsing_rod.tooltip.shift"));
+        } else {
+            pTooltipComponents.add(new TranslatableComponent("tooltip.testmod.dowsing_rod.tooltip"));
+
+        }
+
+    }
+
     private void outputValuableCoordinates(BlockPos blockPos, Player player, Block blockBelow) {
         player.sendMessage(new TextComponent("Found " + blockBelow.asItem().getRegistryName().toString() + " at " +
                 "(" + blockPos.getX() + ", " + blockPos.getY() + "," + blockPos.getZ() + ")"), player.getUUID());
     }
 
     private boolean isValuableBlock(Block block) {
-        return block == Blocks.COAL_ORE || block == Blocks.COPPER_ORE
-                || block == Blocks.DIAMOND_ORE || block == Blocks.IRON_ORE;
+        return Registry.BLOCK.getHolderOrThrow(Registry.BLOCK.getResourceKey(block).get()).is(ModTags.Blocks.DOWSING_ROD_VALUABLES);
     }
 
 
